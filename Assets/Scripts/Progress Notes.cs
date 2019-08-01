85422c2b8cb89bec4c1a51e184b2274d7d769de4
Erick <wakayu@unlv.nevada.edu>
2019-08-01 13:54:13 -0700

    Background Audio Addition

---
+++ Assets/Scripts/Progress Notes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressNotes : MonoBehaviour
+{
+  /*
+   * After creating more levels, fix scene indexing
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   *
+   * 
+   */
+}
---
+++ Assets/Scripts/collision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class collision : MonoBehaviour 
+{
+    int curr_scene;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+        curr_scene = SceneManager.GetActiveScene().buildIndex;
+        print(curr_scene); //Fix after making more levels
+
+
+		if (other.tag == "cloud_or_barrier") {
+			SceneManager.LoadScene ("Failure");
+		}
+		if (other.tag == "finishline") {
+			SceneManager.LoadScene ("End");
+		}
+	}
+}
